b11797de562bceff083d8ed84af3be6e26cfdb01
jimmyedelacruz <jimmyedelacruz872@gmail.com>
2026-02-26 23:12:17 +0800

    Select using arithmetic operation

--- Basic_Queries _SELECT_DISTINCT.sql
+++ Basic_Queries _SELECT_DISTINCT.sql
@@ -24,3 +24,6 @@ SELECT DISTINCT Department, Salary FROM Employees;
 
 -- Example 9 – Select with string functions
 SELECT UPPER(FirstName) AS UpperName FROM Employees;
+
+-- Example 10 – Select using arithmetic operation
+SELECT FirstName, Salary, Salary * 12 AS AnnualSalary FROM Employees;

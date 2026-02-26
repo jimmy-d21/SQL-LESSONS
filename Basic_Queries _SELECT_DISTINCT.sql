a94f55f64fd37d28ca6bd428722120ebdca39cef
jimmyedelacruz <jimmyedelacruz872@gmail.com>
2026-02-26 23:10:04 +0800

    Select distinct departments

--- Basic_Queries _SELECT_DISTINCT.sql
+++ Basic_Queries _SELECT_DISTINCT.sql
@@ -3,3 +3,6 @@ SELECT * FROM Employees;
 
 -- Example 2 – Select specific columns
 SELECT FirstName, LastName, Salary FROM Employees;
+
+-- Example 3 – Select distinct departments
+SELECT DISTINCT Department FROM Employees;

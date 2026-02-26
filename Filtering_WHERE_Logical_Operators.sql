da330d14da225058d2d48a450eebbb38abb5610c
jimmyedelacruz <jimmyedelacruz872@gmail.com>
2026-02-26 23:13:36 +0800

    WHERE with comparison

--- Filtering_WHERE_Logical_Operators.sql
+++ Filtering_WHERE_Logical_Operators.sql
@@ -1,2 +1,5 @@
--- Example 11 – Simple WHERE
+-- Example 1 – Simple WHERE
 SELECT * FROM Employees WHERE Department = 'IT';
+
+-- Example 2 – WHERE with comparison
+SELECT * FROM Employees WHERE Salary > 6000;

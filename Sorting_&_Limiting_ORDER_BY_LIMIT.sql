da1fa27238e07d9e1a0804409287b3e700fa928f
jimmyedelacruz <jimmyedelacruz872@gmail.com>
2026-02-27 23:23:36 +0800

    ORDER BY multiple columns

--- Sorting_&_Limiting_ORDER_BY_LIMIT.sql
+++ Sorting_&_Limiting_ORDER_BY_LIMIT.sql
@@ -4,3 +4,5 @@ SELECT * FROM Employees ORDER BY Salary ASC;
 -- Example 2 – ORDER BY Salary descending
 SELECT * FROM Employees ORDER BY Salary DESC;
 
+-- Example 3 – ORDER BY multiple columns
+SELECT * FROM Employees ORDER BY Department ASC, Salary DESC;

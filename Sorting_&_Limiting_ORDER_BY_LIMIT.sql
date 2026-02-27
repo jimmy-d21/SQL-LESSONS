213973aff75ac6319fb7dc9d308bcddef6c450e1
jimmyedelacruz <jimmyedelacruz872@gmail.com>
2026-02-27 23:23:17 +0800

    ORDER BY Salary descending

--- Sorting_&_Limiting_ORDER_BY_LIMIT.sql
+++ Sorting_&_Limiting_ORDER_BY_LIMIT.sql
@@ -1,3 +1,6 @@
--- Example 21 – ORDER BY Salary ascending
+-- Example 1 – ORDER BY Salary ascending
 SELECT * FROM Employees ORDER BY Salary ASC;
+
+-- Example 2 – ORDER BY Salary descending
+SELECT * FROM Employees ORDER BY Salary DESC;
 

3993742abebd1bda361efa426af994298d0867e4
jimmyedelacruz <jimmyedelacruz872@gmail.com>
2026-02-27 23:24:32 +0800

    ORDER BY with OFFSET (skip rows)

--- Sorting_&_Limiting_ORDER_BY_LIMIT.sql
+++ Sorting_&_Limiting_ORDER_BY_LIMIT.sql
@@ -12,3 +12,6 @@ SELECT FirstName, Salary AS MonthlySalary FROM Employees ORDER BY MonthlySalary 
 
 -- Example 5 – ORDER BY with LIMIT (Top 2 salaries)
 SELECT * FROM Employees ORDER BY Salary DESC LIMIT 2;
+
+-- Example 6 – ORDER BY with OFFSET (skip rows)
+SELECT * FROM Employees ORDER BY Salary DESC LIMIT 2 OFFSET 1;

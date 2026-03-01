21e7282309ad5843f7c14376a8dccc58ccc8112e
jimmyedelacruz <jimmyedelacruz872@gmail.com>
2026-03-01 23:37:48 +0800

    COUNT with condition

--- Aggregation_SUM_COUNT_AVG_MIN_MAX.sql
+++ Aggregation_SUM_COUNT_AVG_MIN_MAX.sql
@@ -1,2 +1,5 @@
 -- Example 1 – COUNT total employees
 SELECT COUNT(*) AS TotalEmployees FROM Employees;
+
+-- Example 2 – COUNT with condition
+SELECT COUNT(*) AS ITEmployees FROM Employees WHERE Department = 'IT';

fea002001b5c4a148c7c06b59a11f770927f84e9
jimmyedelacruz <jimmyedelacruz872@gmail.com>
2026-03-01 23:38:03 +0800

    SUM of all salaries

--- Aggregation_SUM_COUNT_AVG_MIN_MAX.sql
+++ Aggregation_SUM_COUNT_AVG_MIN_MAX.sql
@@ -3,3 +3,6 @@ SELECT COUNT(*) AS TotalEmployees FROM Employees;
 
 -- Example 2 – COUNT with condition
 SELECT COUNT(*) AS ITEmployees FROM Employees WHERE Department = 'IT';
+
+-- Example 3 – SUM of all salaries
+SELECT SUM(Salary) AS TotalSalary FROM Employees;

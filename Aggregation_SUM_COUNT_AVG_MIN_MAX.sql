8208272929940e7984dfd39cc2aa848d31c9626e
jimmyedelacruz <jimmyedelacruz872@gmail.com>
2026-03-01 23:38:41 +0800

    MIN salary

--- Aggregation_SUM_COUNT_AVG_MIN_MAX.sql
+++ Aggregation_SUM_COUNT_AVG_MIN_MAX.sql
@@ -9,3 +9,6 @@ SELECT SUM(Salary) AS TotalSalary FROM Employees;
 
 -- Example 4 – AVG salary
 SELECT AVG(Salary) AS AvgSalary FROM Employees;
+
+-- Example 5 – MIN salary
+SELECT MIN(Salary) AS MinSalary FROM Employees;

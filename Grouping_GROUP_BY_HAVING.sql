14571e6afdac50ba837bd7ea103e4512f2035778
jimmyedelacruz <jimmyedelacruz872@gmail.com>
2026-03-02 21:47:20 +0800

    GROUP BY with AVG and HAVING

--- Grouping_GROUP_BY_HAVING.sql
+++ Grouping_GROUP_BY_HAVING.sql
@@ -3,3 +3,9 @@ SELECT Department, COUNT(*) AS NumEmployees FROM Employees GROUP BY Department;
 
 -- Example 2 – GROUP BY with SUM
 SELECT Department, SUM(Salary) AS TotalSalary FROM Employees GROUP BY Department;
+
+-- Example 3 – GROUP BY with AVG and HAVING
+SELECT Department, AVG(Salary) AS AvgSalary
+FROM Employees
+GROUP BY Department
+HAVING AVG(Salary) >= 6000;

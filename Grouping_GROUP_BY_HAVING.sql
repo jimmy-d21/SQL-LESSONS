649a1f06dbcc882b0cb18c9959762d402276c30a
jimmyedelacruz <jimmyedelacruz872@gmail.com>
2026-03-02 21:46:58 +0800

    GROUP BY with SUM

--- Grouping_GROUP_BY_HAVING.sql
+++ Grouping_GROUP_BY_HAVING.sql
@@ -1,2 +1,5 @@
 -- Example 1 – GROUP BY department
 SELECT Department, COUNT(*) AS NumEmployees FROM Employees GROUP BY Department;
+
+-- Example 2 – GROUP BY with SUM
+SELECT Department, SUM(Salary) AS TotalSalary FROM Employees GROUP BY Department;

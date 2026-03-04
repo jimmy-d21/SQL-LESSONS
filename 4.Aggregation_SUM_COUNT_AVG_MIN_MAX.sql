6615b3183ac67cd37457469faf773a3ae0b6c789
jimmyedelacruz <jimmyedelacruz872@gmail.com>
2026-03-04 23:36:59 +0800

    COUNT with condition - Results

--- 4.Aggregation_SUM_COUNT_AVG_MIN_MAX.sql
+++ 4.Aggregation_SUM_COUNT_AVG_MIN_MAX.sql
@@ -17,6 +17,10 @@ SELECT COUNT(*) AS TotalEmployees FROM Employees;
 
 -- Example 2 – COUNT with condition
 SELECT COUNT(*) AS ITEmployees FROM Employees WHERE Department = 'IT';
+| ITEmployees |
+| ----------- |
+| 2           |
+
 
 -- Example 3 – SUM of all salaries
 SELECT SUM(Salary) AS TotalSalary FROM Employees;

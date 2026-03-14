4e788a0fd16ff1f048b54c2ae67e4632086009ad
jimmyedelacruz <jimmyedelacruz872@gmail.com>
2026-03-14 23:47:58 +0800

    Join Tables - Table List

---
+++ INNER_JOIN.sql
@@ -0,0 +1,16 @@
+-- Employees
+| EmployeeID | FirstName | DepartmentID | Salary |
+| ---------- | --------- | ------------ | ------ |
+| 1          | John      | 1            | 5000   |
+| 2          | Jane      | 2            | 6000   |
+| 3          | Alice     | 2            | 7000   |
+| 4          | Bob       | 3            | 5500   |
+| 5          | Eve       | NULL         | 8000   |
+
+-- Departments
+| DepartmentID | DepartmentName |
+| ------------ | -------------- |
+| 1            | HR             |
+| 2            | IT             |
+| 3            | Finance        |
+| 4            | Marketing      |
